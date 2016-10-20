e557514702d7f6010204bd518330cb6643abd603
sphilippe7 <solomon924@gmail.com>
2016-10-20 09:16:51 -0400

    Diagnostic on 10/20
    see pull request description

---
+++ diagnostic.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Reuse functions wherever possible.
+
+// 1. Write a function that takes a string argument
+// splits it into an array of normalized words
+// (uppercase strings without punctuation)
+// and returns that array.
+const normalizeWords = function () {
+
+};
+
+// 2. Write a function that takes a string argument
+// and returns an array of unique normalized words.
+const uniqueWords = function (/* */) {
+  /* your response here */
+};
+
+// 3. Write a function that takes a string argument and returns the count of
+// words in a string.
+// Provide the *option* to count unique words instead of total words, by passing
+// a second argument.
+
+const wordCount = function (/* */) {
+  /* your response here */
+};
+
+// 4. Write a function that takes a string and returns a dictionary with unique
+// words as keys and a count of each word as the value for that key
+const wordFrequencies = function (/* */) {
+  /* your response here */
+};
+
+module.exports = {
+  normalizeWords,
+  uniqueWords,
+  wordCount,
+  wordFrequencies,
+};
